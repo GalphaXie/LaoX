315b89dba05a523b047b508fd65cc1d55f8c134f
guang.xie@boonry.com <guang.xie@boonray.com>
2019-10-01 21:01:39 +0800

    django ORM 未解决的问题

--- Django_/django_orm_/__init__.py
+++ Django_/django_orm_/__init__.py
@@ -4,3 +4,17 @@
 # description:
 
 # *-* coding:utf8 *-*
+
+
+"""
+关于: 1. update + F 来高效率的 进行 字符串 类型的数据库字段 修改问题 的研究:
+
+参考资料:
+
+https://docs.djangoproject.com/en/2.2/ref/models/database-functions/#concat
+
+django safedelete  实现
+
+避免竞争, 可以加个锁.
+
+"""
